e0c5b59c3a3ebee97002f8660226085ac05e2896
Gabryel Silva <gabryelssantos@gmail.com>
2022-12-15 16:30:13 -0300

    Versão 1.0.8

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -1,17 +1,15 @@
-
 const controls = document.querySelectorAll('.control');
 let currentItem = 0;
 const items = document.querySelectorAll('.item');
 const maxItems = items.length;
-let width = screen.width;
 
 controls.forEach((control) => {
     control.addEventListener('click', () => {
         const isLeft = control.classList.contains("arrow-left");
         if(isLeft){
-            currentItem -= 1;
+            currentItem -= 4;
         } else{
-            currentItem += 1;
+            currentItem += 4;
         }
         if(currentItem >= maxItems){
             currentItem = 0;
@@ -19,11 +17,9 @@ controls.forEach((control) => {
         if(currentItem < 0){
             currentItem = maxItems - 1;
         }
-        items.forEach(item => item.classList.remove('current-item'));
         items[currentItem].scrollIntoView({
             inline: "center",
             behavior: "smooth"
-        })
-        items[currentItem].classList.add("current-item");
+        });
     });
 });
